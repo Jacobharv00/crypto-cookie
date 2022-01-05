246cf97dc665f55579e7d2c5eaae9c8cacb817a5
jacob <jacobharv00@gmail.com>
2022-01-05 15:34:39 -0700

    setup for blockchain smart contract

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,12 @@
+require( '@nomiclabs/hardhat-waffle' )
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/pQq3NoG25KH_hK0oYYNhGHvRRhAUcALh',
+      accounts: [ '00958f5ac7d81493808448d38634f24ede28a79ae0f9df6ec0126afba1ef99bb' ]
+    }
+  }
+
+}
