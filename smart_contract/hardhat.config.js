3f9aafea43435ae793bbc2e13500c75c1ff797ec
jacob <jacobharv00@gmail.com>
2022-01-10 21:39:50 -0700

    redid smart contract

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,7 +4,7 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/pQq3NoG25KH_hK0oYYNhGHvRRhAUcALh',
+      url: 'https://eth-ropsten.alchemyapi.io/v2/4uMTOHf2lOci8XkDXYZSX8TYKgrWKs6K',
       accounts: [ '00958f5ac7d81493808448d38634f24ede28a79ae0f9df6ec0126afba1ef99bb' ]
     },
   }
